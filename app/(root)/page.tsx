c6bcdcd36e1c0ddb057fad6f1ff98d49696137c0
mitanshu <mitanshubadgujar8@gmail.com>
2024-02-28 16:05:38 +0530

    Initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { UserButton } from "@clerk/nextjs";
+
+const Home = () => {
+  return (
+    <div className="h-screen">
+      <p>HOME</p>
+ 
+  </div>
+
+  )
+}
+
+export default Home
